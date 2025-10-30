bd37ad4ae5d03ef656d5ee66a7a5b223ee92d88e
JeromeMendoza123 <c1778-23@itmlyceumalabang.onmicrosoft.com>
2025-10-30 22:07:29 +0800

    Dependency Injection set up and db file creation

--- BSIT31A1-Mendoza-MarketPlace/Program.cs
+++ BSIT31A1-Mendoza-MarketPlace/Program.cs
@@ -1,3 +1,4 @@
+using BSIT31A1_Mendoza_MarketPlace.Infrastructure;
 using BSIT31A1_Mendoza_MarketPlace.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -7,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(connectionString));
+builder.Services.AddDbContext<MarketplaceDbContext>(options =>
     options.UseSqlite(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
