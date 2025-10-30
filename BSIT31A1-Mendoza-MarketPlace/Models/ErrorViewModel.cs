b1c13e0a93900df01a81e7e804d14939d1bb28b9
JeromeMendoza123 <c1778-23@itmlyceumalabang.onmicrosoft.com>
2025-10-30 21:41:17 +0800

    Add project files.

---
+++ BSIT31A1-Mendoza-MarketPlace/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BSIT31A1_Mendoza_MarketPlace.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
